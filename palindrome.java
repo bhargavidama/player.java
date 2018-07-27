0cb885abbe0a73a277741c963087a7365ebe6af3
bhargavidama <41729354+bhargavidama@users.noreply.github.com>
2018-07-27 19:28:01 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int num, reversedInteger = 0, remainder, originalInteger;
+	Scanner sc=new Scanner(System.in);
+	num=sc.nextInt();
+
+        originalInteger = num;
+
+        // reversed integer is stored in variable 
+        while( num != 0 )
+        {
+            remainder = num % 10;
+            reversedInteger = reversedInteger * 10 + remainder;
+            num  /= 10;
+        }
+
+        // palindrome if orignalInteger and reversedInteger are equal
+        if (originalInteger == reversedInteger)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
