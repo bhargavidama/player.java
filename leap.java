af0968f63c707953d63fadb9c2597e0b458d8463
bhargavidama <41729354+bhargavidama@users.noreply.github.com>
2018-07-27 12:00:28 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,39 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+        int year;
+        Scanner s = new Scanner(System.in);
+        year = s.nextInt();
+        boolean leap = false;
+
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+                // year is divisible by 400, hence the year is a leap year
+                if ( year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+            else
+                leap = true;
+        }
+        else
+            leap = false;
+
+        if(leap)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
